ba7b11d3c72dfd2a484a9cb3e50513bb45847435
Thomas Stockx <thomas.stockx@gmail.com>
2013-07-03 16:04:27 +0200

    Adding documentation before going public

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
-﻿// Node.js entry point
+﻿// @author: Thomas Stockx
+// @copyright: OKFN Belgium
+
+// Node.js entry point
 
 var express = require("express");
 var spots = require("./urlroutes/spots");
 
 var app = express();
 
+// define the spots API url routes.
 app.get("/spots", spots.findAll);
 app.get("/spots/:id", spots.findById);
 
 
 console.log("Listening on port 1337...");
+// start server
 app.listen(1337);
