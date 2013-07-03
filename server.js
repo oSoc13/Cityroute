9503ac2757a75cbe6ccf2fd7b382d91642e8e529
Thomas Stockx <thomas.stockx@gmail.com>
2013-07-03 15:59:32 +0200

    Hello world API!

---
+++ server.js
@@ -0,0 +1,13 @@
+﻿// Node.js entry point
+
+var express = require("express");
+var spots = require("./urlroutes/spots");
+
+var app = express();
+
+app.get("/spots", spots.findAll);
+app.get("/spots/:id", spots.findById);
+
+
+console.log("Listening on port 1337...");
+app.listen(1337);
---
+++ urlroutes/spots.js
@@ -0,0 +1,7 @@
+﻿exports.findAll = function (request, response) {
+    response.send([{ name: 'spot1' }, { name: 'spot2' }, { name: 'spot' }]);
+};
+
+exports.findById = function (request, response) {
+    response.send({ id: request.params.id, name: "spot1", description: "description" });
+};
