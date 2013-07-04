f654114b22fffa3909c49f0f874d9dda1ea2dab0
Mathias <mathiasraets@gmail.com>
2013-07-04 15:02:48 +0200

    Weird errors in Chrome and FF

--- clientpage/js/geo.js
+++ clientpage/js/geo.js
@@ -3,11 +3,12 @@
 
 //acquire geolocation
 $(document).ready(function(){
+    
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(showPosition);
     }
     else {
-        $("#geolocationPar").text("Geolocation is not supported by this browser.");
+        $("#geolocationPar").text("Geolocation is not supported by this browser."   );
     }
   });
   
@@ -15,4 +16,23 @@ $(document).ready(function(){
 // callback function
 function showPosition(position) {
     $("#geolocationPar").html("Latitude: " + position.coords.latitude +  "</br>Longitude: " + position.coords.longitude);
+   // var request = "http://localhost:1337/spots?" + "latitude=" +  position.coords.latitude + "&longitude=" +  position.coords.longitude;
+   
+   
+    var url =  "http://10.129.129.67:1337/spots?latitude=12&longitude=12";
+    $.ajax({
+       type: 'GET',
+       crossDomain:true,
+        url: url,
+        success: successSpots,
+        error: function(jqXHR, errorstatus, errorthrown) {
+           alert("Error: " + errorstatus);
+        }
+    });
+}
+
+
+function successSpots(data, textStatus, jqXHR) {
+    alert("Success");
+    $("#geolocationPar").html(data);
 }
---
+++ spots.js
@@ -0,0 +1,12 @@
+﻿// @author: Thomas Stockx
+// @copyright: OKFN Belgium
+
+// return a json with like every spot
+exports.findAll = function (request, response) {
+    response.send([{ name: 'spot1' }, { name: 'spot2' }, { name: 'spot' }]);
+};
+
+// return a json by spot id
+exports.findById = function (request, response) {
+    response.send({ id: request.params.id, name: "spot1", description: "description" });
+};
