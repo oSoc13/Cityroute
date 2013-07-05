f147efbb8e9178eaca3da657bca00575d72c8d89
Mathias <mathiasraets@gmail.com>
2013-07-05 14:53:08 +0200

    Show routes for a given nearby spot.

--- clientpage/js/geo.js
+++ clientpage/js/geo.js
@@ -30,19 +30,50 @@ function showPosition(position) {
            alert("Error: " + errorstatus);
         }
     });
-}
+};
 
 // callback function after the call in showPosition
 function successSpots(data, textStatus, jqXHR) {
-    alert("Success");
-    //$("#geolocationPar").html(data);
     parseSpotList(data);
-}
+};
 
-
+// parse the list of nearby spots and show them in an (ugly) table
 function parseSpotList( spotList ){
     var parsedSpotlist = JSON.parse(spotList);
     $.each(parsedSpotlist, function(index, value) {
-        $('#spotListTable').append('<tr><td>' + value.title + '</td><td>' + value.meta_info.distance_str + '</td></tr>');
+        $('#spotListTable').append('<tr><td>' + value.title + '</td><td>' + value.meta_info.distance_str + 
+        '</td><td> <input type="button" onclick=showRoute("' + value.link.params.id + '") value="Show route" /></tr>');
     });    
-}
+};
+
+// show a list of possible routes for a given spot ID
+// @param spotID: the ID of the spot
+function showRoute ( spotID ){
+    // send a request to the nodeJS API to acquire the nearby spots
+   // parameters: latitude and longitude
+   // returns: list of spots
+    var url =  "http://localhost:1337/routes/?spot_id=" + spotID;
+    $.ajax({
+       type: 'GET',
+       crossDomain:true,
+        url: url,
+        success: successRoute,
+        error: function(jqXHR, errorstatus, errorthrown) {
+           alert("Error: " + errorstatus);
+        }
+    });
+};
+
+// callback function after requesting the routes for a spot
+function successRoute(data, textStatus, jqXHR) {    
+    $("#routes").html("");
+    // for each route
+    $.each(data, function (routeIndex, routeValue) {
+        $("#routes").append("<table><th><td>Route " + routeValue.name + "</th></td>");
+        // for each spot
+        $.each(routeValue.points, function(spotIndex, spotValue) {
+            $("#routes").append("<tr><td>Spot:  " + spotValue.item + "</td></tr>");
+        });
+        $("#routes").append("</table>");
+    });
+};
