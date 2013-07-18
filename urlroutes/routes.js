3978b234d50311f9285300b9a5bb61b3904d74bf
Thomas Stockx <thomas.stockx@gmail.com>
2013-07-18 10:42:32 +0200

    added lots of documentation to routes.js

--- urlroutes/routes.js
+++ urlroutes/routes.js
@@ -9,6 +9,7 @@
  * @return json representation of the Routes
  */
 exports.findRoutesStartingAtSpot = function (request, response) {
+    // declare external files
     var utils = require("../utils");
     var querystring = require('querystring');
     var mongojs = require('mongojs');
@@ -19,10 +20,12 @@ exports.findRoutesStartingAtSpot = function (request, response) {
     var collection = db.collection(config.collection);
 
     // check for url parameters, spot_id should be defined
-    if (typeof request.query.spot_id !== undefined) {   
+    if (typeof request.query.spot_id !== undefined) {
+
+        // parse spot_id to an integer to avoid malicious attempts
         var spot_id_safe = parseInt(request.query.spot_id);
-        // find all routes which have item x as starting point.
-        //console.log(request.query.spot_id);
+
+        // find all routes which have item x as starting point
         require('mongodb').connect(server.mongourl, function (err, conn) {
             collection.find({ 'points.0': { item: request.query.spot_id } })
                 .toArray(function (err, docs) {
@@ -34,6 +37,7 @@ exports.findRoutesStartingAtSpot = function (request, response) {
                         });
                     }
                     else {
+                        // find all routes which have item x as ending point
                         collection.find({ $where: 'this.points[this.points.length-1].item == ' + spot_id_safe })
                             .toArray(function (err, docs2) {
                                 if (err) {
@@ -66,16 +70,33 @@ exports.findRoutesStartingAtSpot = function (request, response) {
 }
 
 /**
- * lat long spot_id channel_name radius
+ * Store a generated route in the database based on the given parameters
+ * @param channelname name of the channel to use for generation
+ * @param token bearer_token of the session
+ * @param latitude latitude of the location
+ * @param longitude longitude of the location
+ * @param spot_id id of the starting spot
+ * @param radius radius to search for each next spot (in km)
+ * @return json representation of the generated route.
  */
 exports.generateRoute = function (request, response) {
+    // declare external files
     var spotsFile = require("./spots");
 
+    // check for invalid request
     if (typeof request.query.token !== undefined && typeof request.query.latitude !== undefined && typeof request.query.longitude !== undefined && typeof request.query.spot_id !== undefined && typeof request.query.radius !== undefined) {
+        // start the route with an array containing the starting spot
         jsonResult = [{
             "item": "" + parseInt(request.query.spot_id)
         }];
+        // this function contains the algorithm to generate the route
         spotsFile.findSpotByChannel(request.query.latitude, request.query.longitude, request.params.channelname, request.query.radius, request.query.spot_id, jsonResult, response);
+    } else {
+        // bad request
+        response.send({
+            "meta": utils.createErrorMeta(400, "X_001", "The 'spot_id', 'token', 'latitude', 'longitude' or 'radius' has no data and doesn't allow a default or null value."),
+            "response": {}
+        });
     }
     
 }
@@ -88,11 +109,19 @@ exports.generateRoute = function (request, response) {
 exports.findById = function (request, response) {
     var mongojs = require('mongojs');
     var ObjectId = mongojs.ObjectId;
+    // search the route in the database and don't edit anything.
     searchById(ObjectId(request.params.id), response, true);
 }
 
+/**
+ * Search for routes by an id.
+ * @param id id of the route
+ * @param response allows this function to return the response to the original request
+ * @param returnResponse true if normal call, false if the static map png still needs to be generated and added
+ */
 searchById = function(id, response, returnResponse)
 {
+    // declare external files
     var utils = require("../utils");
     var mongojs = require('mongojs');
     var config = require('../auth/dbconfig');
@@ -118,6 +147,7 @@ searchById = function(id, response, returnResponse)
                     });
                 } else if (!docs) {
                     // we visited all docs in the collection
+                    // if docs is empty
                     if (resultAmount == 0) {
                         response.send({
                             "meta": utils.createErrorMeta(400, "X_001", "The ID was not found. " + err),
@@ -125,6 +155,7 @@ searchById = function(id, response, returnResponse)
                         });
                     }
                 } else {
+                    // increase resultAmount so on next iteration the algorithm knows the id was found.
                     resultAmount++;
                     // this contains the JSON array with spots
                     var spotArray = docs.points;
@@ -133,7 +164,7 @@ searchById = function(id, response, returnResponse)
                     var resultArray = [];
                     var spotsIdArray = [];
 
-                    // create a basic array (with no JSON content) containing the spot urls in the right order
+                    // create a array containing the spot urls in the right order
                     for (var i = 0; i < spotArray.length; ++i) {
                         spotsIdArray[i] = parseInt(spotArray[i].item);
                     }
@@ -153,6 +184,7 @@ searchById = function(id, response, returnResponse)
                                     "response": {}
                                 });
                             } else {
+                                // for each spot, parse the result
                                 parseRouteSpots(error, responselib, body, resultArray, spotArray, spotsIdArray, count, docs, response, returnResponse);
                                 count++;
                             }
@@ -176,6 +208,7 @@ searchById = function(id, response, returnResponse)
  * @param response used to create a response to the client
  */
 parseRouteSpots = function (error, responselib, body, resultArray, spotArray, spotsIdArray, count, docs, response, returnResponse) {
+    // delcare external files
     var requestlib = require('request');
     var gm = require('../lib/googlemaps');
     var utils = require('../utils');
@@ -184,13 +217,11 @@ parseRouteSpots = function (error, responselib, body, resultArray, spotArray, sp
     var jsonResult = JSON.parse(body);
 
     // insert the results in the correct order as they are defined by a route.
-    // resultArray[spotsIdArray.indexOf(parseInt(jsonResult.response.id))] = jsonResult;
     for (var i = 0; i < spotsIdArray.length; ++i) {
         if (spotsIdArray[i] == parseInt(jsonResult.response.id)) {
             resultArray[i] = jsonResult;
         }
     }
-
 
     // if all external API calls are returned, respond with the ordered JSON array.
     // also included are the name and the id of the route.
@@ -203,7 +234,8 @@ parseRouteSpots = function (error, responselib, body, resultArray, spotArray, sp
         for (var j = 0; j < spotArray.length; ++j) {
             markers[j] = { 'label': j+1, 'location': resultArray[j].response.latitude + " " + resultArray[j].response.longitude };
         }
-                              
+                
+        // define the number of spots and the waypoints string
         var numSpots = resultArray.length - 1;
         var waypoints = "";
 
@@ -216,7 +248,7 @@ parseRouteSpots = function (error, responselib, body, resultArray, spotArray, sp
         var latLong = originLat + ", " + originLong;
         var destLatLong = destLat + ", " + destLong;
 
-        // fill waypoint array with spots between start and endpoint
+        // fill waypoint string with spots between start and endpoint
         for (var i = 1; i < numSpots; ++i) {
             waypoints += resultArray[i].response.latitude + ", " + resultArray[i].response.longitude + "|";
         }
@@ -242,6 +274,7 @@ parseRouteSpots = function (error, responselib, body, resultArray, spotArray, sp
                     "response": {}
                 });
             } else {
+                // parse the results of the Google Maps Directions API
                 parseDirectionResults(error, responselib, body, resultArray, markers, docs, response, returnResponse);
             }
         });
@@ -259,6 +292,7 @@ parseRouteSpots = function (error, responselib, body, resultArray, spotArray, sp
  * @param response used to create a response to the client
  */
 parseDirectionResults = function (error, responselib, body, resultArray, markers, docs, response, returnResponse) {
+    // declare external files
     var polyline = require('polyline');
     var gm = require('../lib/googlemaps');
     var config = require('../auth/dbconfig');
@@ -269,6 +303,7 @@ parseDirectionResults = function (error, responselib, body, resultArray, markers
     var db = mongojs(config.dbname);
     var collection = db.collection(config.collection);
 
+    // parse the result of the Google Directions API to a JSON object
     var jsonResult = JSON.parse(body);
 
     // decode the polyline representation of the route to a readable array of lat and longs.
@@ -276,7 +311,7 @@ parseDirectionResults = function (error, responselib, body, resultArray, markers
     var paths = [];
     paths[0] = { 'points': points };
 
-    // send the response to the user, it contains a link to a static png with a map view on Google Maps
+    // if normal request, send the response to the user
     if (returnResponse) {
         response.send({
             "meta": utils.createOKMeta(),
@@ -290,10 +325,12 @@ parseDirectionResults = function (error, responselib, body, resultArray, markers
         });
     }
     else {
+        // The static map png still has to be generated first
         var db = mongojs(config.dbname);
         var collection = db.collection(config.collection);
         var ObjectId = mongojs.ObjectId;
 
+        // find the route by its id, generate a static map png and add it to the database
         require('mongodb').connect(server.mongourl, function (err, conn) {
             collection.update(
                 { '_id': docs._id },
@@ -319,6 +356,7 @@ parseDirectionResults = function (error, responselib, body, resultArray, markers
                             "response": {}
                         });
                     } else {
+                        // return the route including its png
                         response.send({
                             "meta": utils.createOKMeta(),
                             "response": {
@@ -354,13 +392,17 @@ parseDirectionResults = function (error, responselib, body, resultArray, markers
  @return the route id
  */
 exports.addRoute = function (request, response) {
+    // declare external files
     var mongojs = require('mongojs');
     var config = require('../auth/dbconfig');
     var server = require('../server');
     var utils = require('../utils');
+
     var db = mongojs(config.dbname);
     var collection = db.collection(config.collection);
+
     require('mongodb').connect(server.mongourl, function (err, conn) {
+        // insert the route in the database
         collection.insert({
             "name": request.body.name,
             "description": request.body.description,
@@ -372,10 +414,12 @@ exports.addRoute = function (request, response) {
                     "response": {}
                 });
             } else {
+                // this function returns a result to the user, but a boolean is set so the static map png will be generated first.
                 searchById(docs[0]._id, response, false);
             }
         });
     });
 };
 
+// searchById should be usable from other files
 exports.searchById = searchById;
